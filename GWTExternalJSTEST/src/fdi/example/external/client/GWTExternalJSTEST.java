81a7249b3e3ab16446c9df4d01320fe06557b8ab
Joaquin Gayoso Cabada <gayoxo@gmail.com>
2017-10-24 20:32:41 +0200

    Soy La pera limonera

--- GWTExternalJSTEST/src/fdi/example/external/client/GWTExternalJSTEST.java
+++ GWTExternalJSTEST/src/fdi/example/external/client/GWTExternalJSTEST.java
@@ -3,7 +3,10 @@ package fdi.example.external.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.shared.GWT;
-import com.google.gwt.user.client.Window;
+//import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -14,7 +17,31 @@ public class GWTExternalJSTEST implements EntryPoint {
         export();
     }
 	
+	public GWTExternalJSTEST() {
+		
+	}
 	
+	
+	public GWTExternalJSTEST(String id) {
+		RootPanel RP=RootPanel.get(id);
+		VerticalPanel VP=new VerticalPanel();
+		RP.add(VP);
+		
+		ListBox LB=new ListBox();
+		LB.setMultipleSelect(true);
+		LB.addItem("primero");
+		LB.addItem("primero1");
+		LB.addItem("primero2");
+		LB.addItem("primero3");
+		LB.addItem("primero4");
+		LB.addItem("primero5");
+		LB.addItem("primero6");
+		LB.addItem("primero7");
+		LB.addItem("primero8");
+		LB.addItem("primero9");
+		VP.add(LB);
+	}
+
 	/**
      * Makes our setData method accessible from plain JS
      */
@@ -24,8 +51,10 @@ public class GWTExternalJSTEST implements EntryPoint {
 
     private static void setData(JavaScriptObject javaScriptObject) {
     	GWT.log(javaScriptObject.toString());
-    	Window.alert("HolaMundo");
+    //	Window.alert("HolaMundo");
         // this method is now reachable as window.setData
+    	new GWTExternalJSTEST(javaScriptObject.toString());
+   
     }
 
 	/**
